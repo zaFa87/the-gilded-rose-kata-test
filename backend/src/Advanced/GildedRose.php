920506b4941effb82e4e480be1e8f3958a1524ff
fabioos6 <fabio@onlinesucces.nl>
2022-05-10 18:34:30 +0200

    Remove initialization from GildedRose class

--- backend/src/Advanced/GildedRose.php
+++ backend/src/Advanced/GildedRose.php
@@ -76,19 +76,7 @@ class ConjuredItem extends Items
 }
 class GildedRose
 {
-    public $name;
-
-    public $quality;
-
-    public $sellIn;
-
-    public function __construct($name, $quality, $sellIn)
-    {
-        $this->name = $name;
-        $this->quality = $quality;
-        $this->sellIn = $sellIn;
-    }
-
+    
     public static function of($name, $quality, $sellIn) {
         
         if($name == 'Aged Brie')
